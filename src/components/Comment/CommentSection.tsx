9b79f01c18eff9f96cf6aad1e249915578928097
addseven <23041212770@stu.xidian.edu.cn>
2025-09-22 16:34:49 +0800

    fix(types): 修复类型错误，删除不需要的文件与导入2

--- src/components/Comment/CommentSection.tsx
+++ src/components/Comment/CommentSection.tsx
@@ -1,10 +1,10 @@
 // src/components/CommentSection.tsx
-import React, { useState } from "react";
+import  { useState } from "react";
 import type { Comment } from "../../types/comment";
 import CommentList from "./CommentList";
 import CommentForm from "./CommentForm";
 import s from "./Comment.module.scss";
-import { useTheme } from "../../context/ThemeContext";
+// import { useTheme } from "../../context/ThemeContext";
 
 // 定义 props 的类型
 interface CommentSectionProps {
@@ -18,7 +18,7 @@ interface CommentSectionProps {
 
 function CommentSection({ comments, totalCount, onTopLevelSubmit, onReplySubmit }: CommentSectionProps) {
     const [activeReplyId, setActiveReplyId] = useState<string | null>(null);
-    const { primaryColor, setPrimaryColor } = useTheme();
+    // const { primaryColor, setPrimaryColor } = useTheme();
 
     const handleReplySubmitAndCloseForm = (parentId: string, content: string) => {
         onReplySubmit(parentId, content);
